28149c18222e4a1124a8f04af5c6fe968a6480eb
pablode <pablo.delapuente@gmail.com>
2025-08-19 11:17:02 -0300

    Ejercicio 6

--- src/TP2/Ejercicio_6.java
+++ src/TP2/Ejercicio_6.java
@@ -19,6 +19,23 @@ public class Ejercicio_6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        
+        int positivos=0,negativos=0,ceros=0,num=0;
+        for (int i=1; i<11;i++){
+            System.out.println("Ingrese el numero: "+i);
+            num=input.nextInt();
+            if (num<0){
+                negativos+=1;
+            }
+            else if(num>0){
+                positivos+=1;
+            }
+            else ceros+=1
+                    ;
+        }
+        System.out.println(
+            "Resultados:\n"+ 
+            "Positivos: "+positivos+"\n" +
+            "Negativos: "+negativos+"\n" +
+            "Ceros: "+ceros);
     }
 }
